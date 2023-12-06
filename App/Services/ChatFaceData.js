ec07d56f63b547029b7f262b7e8f8eb3fc5f8f00
williamtran207 <quochuyphbrvt07@gmail.com>
2023-12-06 16:24:03 +0700

    update

--- App/Services/ChatFaceData.js
+++ App/Services/ChatFaceData.js
@@ -1,21 +1,21 @@
 export default chatFaceData=[
     {
         id:1,
-        name:'Noyi',
-        image:'https://res.cloudinary.com/dknvsbuyy/image/upload/v1685678135/chat_1_c7eda483e3.png',
-        primary:'#FFC107',
+        name:'Adam',
+        image:'https://res.cloudinary.com/dknvsbuyy/image/upload/v1685709886/image_22_409561b953.png',
+        primary:'#3B96D2',
         secondary:''
     },
     {
         id:2,
-        name:'Pogu',
-        image:'https://res.cloudinary.com/dknvsbuyy/image/upload/v1685709886/image_21_2e18bb4a61.png',
-        primary:'#E53057',
+        name:'Adam',
+        image:'https://res.cloudinary.com/dknvsbuyy/image/upload/v1685709886/image_22_409561b953.png',
+        primary:'#3B96D2',
         secondary:''
     },
     {
         id:3,
-        name:'Nista',
+        name:'Adam',
         image:'https://res.cloudinary.com/dknvsbuyy/image/upload/v1685709886/image_22_409561b953.png',
         primary:'#3B96D2',
         secondary:''
