0e3460f869cdc8ee0c071daece6037e6c3b5fc51
williamtran207 <quochuyphbrvt07@gmail.com>
2024-02-03 06:17:27 +0700

    Update GlobalApi.js

--- App/Services/GlobalApi.js
+++ App/Services/GlobalApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://openai-api-nodejs.onrender.com/app'
+const BASE_URL = 'https://openai-translate-api-nodejs.onrender.com/app'
 // const BASE_URL = 'http://192.168.1.21:3001/app'   //Replace with System PC IP address
 
 const getBardApi=(msg)=>axios.get(BASE_URL+"?ques="+msg);
